a405e0252f72ca2e8feab157f5779e5c416e8492
emonkhan500 <khdilo47@gmail.com>
2025-06-09 20:47:20 +0600

    started working on banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className='h-screen mt-16 md:mt-5'>
-            <h1>This is Banner</h1>
+            <div className='flex flex-col md:flex-row'>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
