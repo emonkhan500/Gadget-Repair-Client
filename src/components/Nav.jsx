5c8a46a4a981047d2a03156776a76e3a03dddb0e
emonkhan500 <khdilo47@gmail.com>
2025-06-09 16:39:17 +0600

    working on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
+
 const Nav = () => {
     const [isScrolled, setIsScrolled] = useState(false);
 
@@ -13,18 +14,45 @@ const Nav = () => {
       window.addEventListener('scroll', handleScroll);
       return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+    const links=[
+      {
+        title:'Home',
+        path:'/'
+      },
+      {
+        title:'Services',
+        path:'/service'
+      },
+    
+      {
+        title:'Book a Repair',
+        path:'/bookrepair'
+      },
+      {
+        title:'Contact',
+        path:'/contact'
+      },
+    ]
   
     return (
       <nav className={`fixed top-0 w-full z-50 transition-all duration-300 ${
-        isScrolled ? 'bg-white shadow-md py-6' : 'bg-transparent py-6'
+        isScrolled ? 'bg-white shadow-md py-6' : 'bg-transparent py-6 '
       }`}>
-        <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
-          <h1 className="text-xl font-bold">MySite</h1>
-          <div className="space-x-4">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+        <div className="flex justify-around " >
+        <div className="w-20 md:w-[130px] lg:w-[160px] xl:w-[180px] ">
+            <img src="/logo.webp" alt="logo" />
           </div>
-        </div>
+
+  <ul className="flex justify-around gap-5">
+   {
+    links.map((link,index)=><li key={index}><Link href={link.path}>{link.title}</Link> </li>)
+   }
+  </ul>
+  <div className='space-x-3'>
+  {/* <button onClick={handleSignIn}>SignIn</button>
+  <button onClick={handleSignUp}>SignUp</button> */}
+  </div>
+</div>
       </nav>
     );
 };
