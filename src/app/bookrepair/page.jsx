0c424428363b581bc4b7fcdf7f77273f31516d01
emonkhan500 <khdilo47@gmail.com>
2025-06-10 19:32:52 +0600

    started working on book repair

--- src/app/bookrepair/page.jsx
+++ src/app/bookrepair/page.jsx
@@ -1,9 +1,22 @@
+import ShareHead from '@/components/ShareHead/ShareHead';
 import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            <h1>this is book page</h1>
+        <div className='relative w-full'>
+            <div
+    className="absolute inset-0 bg-cover bg-center"
+    style={{
+      backgroundImage:
+        "url('https://reactheme.com/products/wordpress/gadgetfix/wp-content/uploads/2024/12/1-1.webp')",
+       // Controls the background opacity
+    }}
+  ></div>
+  <div className="absolute inset-0 bg-black opacity-30 z-0"></div>
+  <div className='relative z-10 text-white pt-6'>
+    <ShareHead upTitle={'What Do We Fix'} pageTitle={'Book a Repair'}></ShareHead>
+  </div>
+            
         </div>
     );
 };
