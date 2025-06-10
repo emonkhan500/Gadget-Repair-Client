f4b7fa167d3cad67eb97c640af0041c4651feda1
emonkhan500 <khdilo47@gmail.com>
2025-06-10 21:13:26 +0600

    working on book repair

--- src/app/bookrepair/page.jsx
+++ src/app/bookrepair/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='relative w-full'>
+        <div className='relative px-16 lg:px-36'>
             <div
     className="absolute inset-0 bg-cover bg-center"
     style={{
@@ -13,8 +13,47 @@ const page = () => {
     }}
   ></div>
   <div className="absolute inset-0 bg-black opacity-30 z-0"></div>
-  <div className='relative z-10 text-white pt-6'>
+  <div className='relative z-10 text-white pt-6 '>
     <ShareHead upTitle={'What Do We Fix'} pageTitle={'Book a Repair'}></ShareHead>
+    
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 p-6 pt-10 pb-36">
+      
+      <div className="text-center space-y-2 border-x px-4">
+        <button className=" text-black rounded-lg  bg-[#C6E76C] px-5 py-1">STEP 1</button>
+        <h3 className="text-2xl font-medium text-white mt-3">
+          Select your repair & request a quote
+        </h3>
+      </div>
+
+      <div className="text-center space-y-2 border-r  px-4">
+        <button className="text-black rounded-lg  bg-[#C6E76C] px-5 py-1">STEP 2</button>
+        <h3 className="text-2xl font-medium text-white mt-3">
+          Send your gadget or visit our store
+        </h3>
+      </div>
+
+      <div className="text-center space-y-2 border-r px-4">
+        <button className="text-black rounded-lg  bg-[#C6E76C] px-5 py-1">STEP 3</button>
+        <h3 className="text-2xl font-medium text-white mt-3">
+          Our technicians fix your gadget
+        </h3>
+      </div>
+
+      <div className="text-center space-y-2 border-r px-4">
+        <button className="text-black rounded-lg  bg-[#C6E76C] px-5 py-1">Step 4</button>
+        <h3 className="text-2xl font-medium text-white mt-3">
+          We'll request payment
+        </h3>
+      </div>
+
+      <div className="text-center space-y-2 border-r px-4">
+        <button className="text-black rounded-lg  bg-[#C6E76C] px-5 py-1">Step 5</button>
+        <h3 className="text-2xl font-medium text-white mt-3 ">
+          Return to you
+        </h3>
+      </div>
+
+    </div>
   </div>
             
         </div>
