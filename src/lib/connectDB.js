3b8a1da8d887de3544fca891490335bfa997042e
emonkhan500 <khdilo47@gmail.com>
2025-06-19 09:42:21 +0600

    working on mongodb

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,3 +1,19 @@
+let db;
 export const connectDB =async ()=>{
+    if(db) return db;
+    try{
+        const uri =process.env.NEXT_PUBLIC_MONGODB_URI
+        const client = new MongoClient(uri, {
+            serverApi: {
+              version: ServerApiVersion.v1,
+              strict: true,
+              deprecationErrors: true,
+            },
+          });
+          db= client.db('gadget-repair')
+          return db;
+        } catch(error){
+            console.log(error);
+        }
 
 }
