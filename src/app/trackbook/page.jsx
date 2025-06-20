c8d22246ef782802fdde4b8b76218a5d9199b34c
emonkhan500 <khdilo47@gmail.com>
2025-06-20 10:38:50 +0600

    design  track your booking

--- src/app/trackbook/page.jsx
+++ src/app/trackbook/page.jsx
@@ -1,9 +1,27 @@
+import ShareHead from '@/components/ShareHead/ShareHead';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            <h1> This is your booking</h1>
+          <div className='relative pt-10 px-4 md:px-16 lg:px-36'>
+            <div
+    className="absolute inset-0 bg-cover bg-center"
+    style={{
+      backgroundImage:
+        "url('https://reactheme.com/products/wordpress/gadgetfix/wp-content/uploads/2024/12/1-1.webp')",
+       // Controls the background opacity
+    }}
+  ></div>
+  <div className="absolute inset-0 bg-black opacity-30 z-0"></div>
+  <div className='relative z-10 text-white pt-6 '>
+    <ShareHead upTitle={'What Do We Fix'} pageTitle={'Track Your Repair'}></ShareHead>
+    
+    
+  </div>
+  
+        </div>
+        
         </div>
     );
 };
