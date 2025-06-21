2f466ab558a1aed0bab5bbd5bd532dfeae9d9334
emonkhan500 <khdilo47@gmail.com>
2025-06-21 11:25:40 +0600

    working on  trackbook

--- src/app/trackbook/page.jsx
+++ src/app/trackbook/page.jsx
@@ -1,5 +1,7 @@
 import ShareHead from '@/components/ShareHead/ShareHead';
 import React from 'react';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
 
 const page = () => {
     return (
@@ -18,7 +20,54 @@ const page = () => {
     <ShareHead upTitle={'What Do We Fix'} pageTitle={'Track Your Repair'}></ShareHead>
     
     
-  <div></div>
+  <div className='pb-16'>
+  <table className="table border-2 border-slate-200 p-3 pb-16">
+        {/* head */}
+        <thead className="text-2xl text-white bg-[#3BB77E] quick ">
+          <tr>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr className="border-b-2 border-slate-100">
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className=" h-24 w-24 border-2 border-slate-100 p-3">
+                    <img
+                      src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="text-lg font-bold text-[#253D4E]">
+              Field Roast Chao Cheese Creamy Original
+            </td>
+            <td className="text-xl font-bold text-[#3BB77E]">$2.5</td>
+            {/* edit */}
+            <th>
+              <button className="btn btn-ghost btn-xl">
+              <CiEdit />
+              </button>
+            </th>
+            <th>
+              <button className="btn btn-ghost btn-lg">
+                <FaRegTrashAlt />
+              </button>
+            </th>
+          </tr>
+         
+         
+        </tbody>
+      </table>
+  </div>
   </div>
   
         </div>
